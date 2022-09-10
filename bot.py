878f82f12fa0964617e7271ef81fcb2f3a7eb0c5
irairam <malikova@gmail.com>
2022-09-10 10:27:13 +0300

    Correct shutdown implemented

--- bot.py
+++ bot.py
@@ -1,21 +1,21 @@
 import logging
 import settings
+# import sys
+import os
+import signal
 from telegram.ext import Updater,  CommandHandler, MessageHandler, Filters
 
 #Включаем логгирование
 logging.basicConfig(filename='bot.log', level=logging.INFO)
 
 def main():
-    # Создаем бота и передаем ему ключ для авторизации на серверах Telegram
     mybot = Updater(settings.API_KEY, use_context=True)
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler('start', greet_user))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
     logging.info('Бот стартовал')
-    # Командуем боту начать ходить в Telegram за сообщениями
     mybot.start_polling()
-    # Запускаем бота, он будет работать, пока мы его не остановим принудительно
     mybot.idle()
 
 def greet_user(update, context):
@@ -25,17 +25,18 @@ def greet_user(update, context):
 
 def talk_to_me(update, context):
     user_text = update.message.text
+    # пробуем застопать бота на определенное сообщение. 
+    # Непонятно как правильно шатдаун сделать
+    # и я не первая. Пробуем решение из https://github.com/python-telegram-bot/python-telegram-bot/issues/801
     if user_text == 'Остановись':
         update.message.reply_text('Так точно, капитан, работа остановлена')
-        exit() #Почему в шелле не останавливает? (
-        # raise SystemExit(0) #какого черта не работает?
+        os.kill(os.getpid(), signal.SIGINT)
+        # sys.exit() #и этот тоже в консоли висит
+        # exit() #В консоли все равно висит
+        # raise SystemExit(0) #в консоли тоже висит
     else: 
         print(user_text)
         update.message.reply_text(user_text)
 
-# Вызываем функцию main(). Неправильный вариант: 
-# main()
-
-#А вот так правильно:
 if __name__ == '__main__':
     main()
